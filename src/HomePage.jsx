1c35633562c3cce5b4161ea77eecb4fa3670b465
helixedra <greencastle08@gmail.com>
2024-10-31 19:36:59 +0100

    markup update

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,23 @@
 function HomePage() {
   return (
     <>
-      <div className="container">HOME PAGE</div>
+      <div className="container">
+        <div className="search">
+          <input type="search" value />
+        </div>
+        <div className="books_slider">
+          <div className="books_slider_header">New Books</div>
+          <div className="books_slider_container">SLIDER</div>
+        </div>
+        <div className="books_categories">
+          <div className="books_categories_header">Browse By Category</div>
+          <div className="books_categories_container">Categories</div>
+        </div>
+        <div className="books_selected">
+          <div className="books_categories_header">Most Listened</div>
+          <div className="books_categories_container">Categories</div>
+        </div>
+      </div>
     </>
   );
 }
