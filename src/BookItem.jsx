8257c4fc6d7af7dd7b04c98d7e725f6bd1a1bbe9
helixedra <greencastle08@gmail.com>
2024-11-01 19:11:49 +0100

    slider

--- src/BookItem.jsx
+++ src/BookItem.jsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
-function BookItem({ data: { id, title, author, cover } }) {
-  return (
-    <>
-      <div className="book_item">
-        <Link to={`/books/${id}`}>
-          <div className="book_cover">
-            <img className="book_cover_s" src={cover} alt="" loading="lazy" />
-          </div>
-          <div className="book_title">{title}</div>
-          <div className="book_author">{author}</div>
-        </Link>
-      </div>
-    </>
-  );
-}
+import { forwardRef } from 'react';
 
+const BookItem = forwardRef(({ data: { id, title, author, cover } }, ref) => {
+  return (
+    <div className="book_item" ref={ref}>
+      <Link to={`/books/${id}`}>
+        <div className="book_cover">
+          <img className="book_cover_s" src={cover} alt="" loading="lazy" />
+        </div>
+        <div className="book_title">{title}</div>
+        <div className="book_author">{author}</div>
+      </Link>
+    </div>
+  );
+});
+BookItem.displayName = 'BookItem';
 export default BookItem;
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,22 +1,112 @@
+import booksData from './books.json';
+import BookItem from './BookItem';
+import { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 function HomePage() {
+  const [slider, setSlider] = useState(0);
+  const [search, setSearch] = useState('');
+  const bookRef = useRef(null);
+  const containerRef = useRef(null);
+
+  function handleSlider(direction) {
+    const slideWidth = bookRef.current
+      ? +bookRef.current.getBoundingClientRect().width
+      : 0;
+
+    setSlider((prevSlider) => {
+      if (direction === 'prev' && prevSlider > 0) {
+        return prevSlider - slideWidth;
+      } else if (direction === 'next') {
+        return prevSlider + slideWidth;
+      }
+      return prevSlider;
+    });
+  }
+
+  // Координати для визначення свайпу
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+  }
+
+  function handleTouchEnd(event) {
+    touchEndX = event.changedTouches[0].clientX;
+    handleSwipe();
+  }
+
+  function handleSwipe() {
+    const slideWidth = bookRef.current
+      ? +bookRef.current.getBoundingClientRect().width
+      : 0;
+    console.log(slideWidth);
+    if (touchEndX < touchStartX) {
+      console.log(slider);
+
+      // Свайп вліво
+      setSlider((prevSlider) => prevSlider + slideWidth);
+    }
+
+    if (touchEndX > touchStartX) {
+      console.log(slider);
+      // Свайп вправо
+      setSlider((prevSlider) => prevSlider - slideWidth);
+    }
+  }
+
   return (
     <>
       <div className="container">
         <div className="search">
-          <input type="search" value />
+          <input
+            type="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search for books..."
+          />
         </div>
-        <div className="books_slider">
-          <div className="books_slider_header">New Books</div>
-          <div className="books_slider_container">SLIDER</div>
+      </div>
+      <div className="books_slider">
+        <div className="books_slider_header">New Books</div>
+        <div className="books_slider_wrapper">
+          <div
+            className="books_slider_container"
+            ref={containerRef}
+            onTouchStart={handleTouchStart}
+            onTouchEnd={handleTouchEnd}
+            style={{ transform: `translateX(-${slider}px)` }}>
+            {booksData.books.map(
+              (book, index) =>
+                index < 10 && (
+                  <BookItem ref={bookRef} data={book} key={book.id} />
+                )
+            )}
+          </div>
         </div>
-        <div className="books_categories">
-          <div className="books_categories_header">Browse By Category</div>
-          <div className="books_categories_container">Categories</div>
+        <div className="books_slider_controls">
+          <button
+            onClick={() => handleSlider('prev')}
+            className="slider_button">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <button
+            onClick={() => handleSlider('next')}
+            className="slider_button">
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
         </div>
-        <div className="books_selected">
-          <div className="books_categories_header">Most Listened</div>
-          <div className="books_categories_container">Categories</div>
-        </div>
+        <div className="books_slider_indicator"></div>
+      </div>
+      <div className="books_categories">
+        <div className="books_categories_header">Browse By Category</div>
+        <div className="books_categories_container">Categories</div>
+      </div>
+      <div className="books_selected">
+        <div className="books_categories_header">Most Listened</div>
+        <div className="books_categories_container">Categories</div>
       </div>
     </>
   );
